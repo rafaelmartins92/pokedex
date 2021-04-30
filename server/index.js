2cf0d0e43c3bfccdb62d3f7ab5161b5da3844ce7
Rafael Martins <rafael.martins92@outlook.com>
2021-04-30 00:24:29 -0300

    chore: connect to database

--- server/index.js
+++ server/index.js
@@ -8,3 +8,19 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://raafs92:mongopassword@cluster0.1iyvv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT} 🚀`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
